ed648e23d899981e1add1debd1a6cd152379efca
Eric Dennison <ericd@netdenizen.com>
2020-08-13 16:34:57 -0400

    Updated from Brython Server: 8/13/2020 4:34:56 PM

--- brytest.py
+++ brytest.py
@@ -1,25 +1,4 @@
 from browser import timer
-
-class NextWrapper:
-    def __init__(self, func, arg):
-        self.func = func
-        self.arg = arg
-
-    def delayed(self):
-        self.func(self.arg)
-        
-    def go(self):
-        timer.set_timeout(self.delayed, 100)
-
-"""
-def inputnext(arg, next):
-    strin = input(arg)
-    n = NextWrapper(next, strin)
-    n.go()
-    #time.set_timeout(next, 1)
-    #t = threading.Thread(target = next, args=(strin,))
-    #t.start()
-"""
     
 class InputNext:
     def __init__(self, prompt, next):
@@ -27,16 +6,15 @@ class InputNext:
        self.next = next
        self.result = None
        print(prompt, end="")
-       timer.set_timeout(self.waitfirst, 0)
+       timer.set_timeout(self._wait_first, 0)
     
-    def waitfirst(self):
+    def _wait_first(self):
         self.result = input()
-        timer.set_timeout(self.waitsecond, 0)
+        timer.set_timeout(self._wait_second, 0)
     
-    def waitsecond(self):
+    def _wait_second(self):
         self.next(self.result)
     
-
 
 def handlein_1(inp):
     print("we received input 1: ", inp)
